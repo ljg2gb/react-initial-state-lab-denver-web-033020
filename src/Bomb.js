e4cf4355728b689422316a0fb4f765f9ccddfce6
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-12 14:19:15 -0600

    simple slider state lab

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+import initialCount from './index.js';
+
+class Bomb extends Component {
+state = {
+    secondsLeft: [this.props.initialCount]
+}
+
+
+
+render() {
+    if(this.state.secondsLeft){
+        return <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+    } else {
+        return <p>Boom!</p>
+    }
+}
+
+}
+
+export default Bomb;
